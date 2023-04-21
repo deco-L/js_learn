378a0f791f6dddce9329774d69daa28f6add94f7
deco-L <scotchwitchbroom@gmail.com>
2023-04-21 16:52:42 +0900

    kari

--- javaScript_base/data_type/data_type.js
+++ javaScript_base/data_type/data_type.js
@@ -1,7 +1,7 @@
 let name = "Ilya";
 
-alert( 'hello ${1}' );
+alert( `hello ${1}` );
 
-alert( 'hello ${"name"}' );
+alert( `hello ${"name"}` );
 
-alert( 'hello ${name}' );
+alert( `hello ${name}` );
