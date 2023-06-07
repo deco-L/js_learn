ce2c9f567502876f7a49635fc7d3abff9e08cf7d
deco-L <scotchwitchbroom@gmail.com>
2023-06-07 17:25:02 +0900

    V V

--- javaScript_base/review/review.js
+++ javaScript_base/review/review.js
@@ -1,1 +1,9 @@
+'use strict'
+
 alert('Hello'); alert('World');
+
+let	userName = prompt("Your name?", "");
+let	isTeaWanted = confirm("Do you want some tea?");
+
+alert( "Visitor: " + userName);
+alert( "Tea wanted: " + isTeaWanted);
